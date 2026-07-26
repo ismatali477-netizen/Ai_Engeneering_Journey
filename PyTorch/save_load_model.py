b4babf7052702d519d38e864151cd98aed5dfa8e
ismatali477-netizen <ismatali477@gmail.com>
2026-07-26 18:32:31 +0545

    add save and load model functionality for MNIST classification using PyTorch

---
+++ PyTorch/save_load_model.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets,transforms
+from torch.utils.data import DataLoader
+transform=transforms.ToTensor()
+train_dataset=datasets.MNIST(
+  root="./data",
+  train=True,
+  download=True,
+  transform=transform
+)
+train_loader=DataLoader(
+    train_dataset,
+    batch_size=64,
+    shuffle=True
+)
+# test_dataset=datasets.MNIST(
+#   root="./data",
+#   train=False,
+#   download=True,
+#   transform=transform
+# )
+# test_loader=DataLoader(
+#     test_dataset,
+#     batch_size=64,
+#     shuffle=False
+# )
+model=nn.Sequential(
+    nn.Conv2d(1,8,3),
+    nn.ReLU(),
+    nn.MaxPool2d(2),
+    nn.Conv2d(8,16,3),
+    nn.ReLU(),
+    nn.Flatten(),
+    nn.Linear(1936,10)
+)
+criterion=nn.CrossEntropyLoss()
+optimizer=optim.Adam(
+    model.parameters(),
+    lr=0.001
+)
+for epoch in range(2):
+  for images,labels in train_loader:
+    output=model(images)
+    loss=criterion(output,labels)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+torch.save(model.state_dict(),"mnist_cnn.pth")
+print("Model Saved Successfully!")
+model = nn.Sequential(
+    nn.Conv2d(1,8,3),
+    nn.ReLU(),
+    nn.MaxPool2d(2),
+    nn.Conv2d(8,16,3),
+    nn.ReLU(),
+    nn.Flatten(),
+    nn.Linear(1936,10)
+)
+model.load_state_dict(torch.load("mnist_cnn.pth"))
+model.eval()
+print("Model Loaded Successfully!")
