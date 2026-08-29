a5431f4ce122d6da754889be50936d6cfd93a937
ismatali477-netizen <ismatali477@gmail.com>
2026-08-29 22:16:35 +0545

    Rename SimpleCNN to CatDogModel; update model architecture

--- PyTorch/Cat_Dog_Classifier/model.py
+++ PyTorch/Cat_Dog_Classifier/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-class SimpleCNN(nn.Module):
+class CatDogModel(nn.Module):
     def __init__(self, num_classes=2):
         super().__init__()
         self.features = nn.Sequential(
