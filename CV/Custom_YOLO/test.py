1243ee6dc651b092df0e0300c245e18502f7d7ac
ismatali477-netizen <ismatali477@gmail.com>
2026-09-07 21:13:43 +0545

    Refactor code structure for improved readability and maintainability

--- CV/Custom_YOLO/test.py
+++ CV/Custom_YOLO/test.py
@@ -1,12 +1,16 @@
 from ultralytics import YOLO
-# Load our trained model
-model = YOLO(
-    r"runs\detect\train\weights\best.pt"
-)
+# Load our custom-trained model
+model = YOLO(r"runs\detect\train-2\weights\best.pt")
 # Test image
 results = model(
     r"C:\Users\Saruk meeya\Downloads\test.jpg",
     conf=0.5
 )
-# Display result
+# Display predictions
 results[0].show()
+# Print predictions
+for box in results[0].boxes:
+    class_id = int(box.cls[0])
+    confidence = float(box.conf[0])
+    class_name = results[0].names[class_id]
+    print(f"Object: {class_name} | Confidence: {confidence:.2f}")
