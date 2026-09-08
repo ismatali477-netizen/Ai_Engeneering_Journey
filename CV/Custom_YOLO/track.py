35287e521a09ac2dc16de48fe3d89b285996db64
ismatali477-netizen <ismatali477@gmail.com>
2026-09-08 21:16:32 +0545

    Add YOLO tracking script for real-time object detection

---
+++ CV/Custom_YOLO/track.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import cv2
+model = YOLO("yolo11n.pt")
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to access webcam")
+        break
+    results = model.track(
+        frame,
+        conf=0.2,
+        persist=True
+    )
+    annotated_frame = results[0].plot()
+    cv2.imshow("YOLO Tracking", annotated_frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
